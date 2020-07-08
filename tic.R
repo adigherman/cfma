1823e85868486ffcf1cfbddf7609d369b5e93c2d
Adi G <adig@jhu.edu>
2020-07-08 12:29:34 -0400

    still trying to build

--- tic.R
+++ tic.R
@@ -1,2 +1,5 @@
+#get_stage("before_install") %>%
+  
+
 # installs dependencies, runs R CMD check, runs covr::codecov()
-do_package_checks(args="--as-cran", build_args=c("--no-manual","binary=TRUE"))
+do_package_checks(args=c("--as-cran","--install-args=--build"))
