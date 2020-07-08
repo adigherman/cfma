c01513832858c401633b0216b7c21d01d4422cf7
Adi G <adig@jhu.edu>
2020-07-08 17:24:48 -0400

    trying to get package version

--- tic.R
+++ tic.R
@@ -1,5 +1,2 @@
-get_stage("before_install") %>%
-  add_code_step(system("export PKG_VERSION=1.0.1"))  
-
 # installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks(args=c("--as-cran","--install-args=--build"))
